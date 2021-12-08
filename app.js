2264f21e4b543bd03614c45f90f01efdd5bd88b5
Jettison <ilmsmjettison@gmail.com>
2021-12-08 21:42:11 +0800

    save

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 let imgBg = document.querySelector('.img-container');
 let arrowRight = document.querySelector('.fa-arrow-right');
-let arrowLeft = docuemnt.querySelector('.fa-arrow-left');
+let arrowLeft = document.querySelector('.fa-arrow-left');
 let counterOne = document.querySelector('.counter');
 let counterTwo = document.querySelector('.counter2');
 let closeNav = document.querySelector('.fa-window-close');
 let openNav = document.querySelector('.fa-bars');
-
-openNav.addEventListener('click', );
-
+let Nav = document.querySelector('.mobile-nav');
 
 
-function showNav(e) {
-    e.target.classList.add('showNav');
+openNav.addEventListener('click', showNav);
+closeNav.addEventListener('click', hideNav);
+
+
+
+function showNav() {
+    Nav.classList.add('showNav');
+}
+
+function hideNav() {
+    Nav.classList.remove('showNav');
 }
