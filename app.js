87aa4f37cb21d6ca8458eaf770bf2f95cba26be7
Jettison <ilmsmjettison@gmail.com>
2021-12-08 20:49:40 +0800

    save

---
+++ app.js
@@ -0,0 +1,5 @@
+let imgBg = document.querySelector('.img-container');
+let arrowRight = document.querySelector('.fa-arrow-right');
+let arrowLeft = docuemnt.querySelector('.fa-arrow-left');
+let counterOne = document.querySelector('.counter');
+let counterTwo = document.querySelector('.counter2');
