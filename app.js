d0c2a8f5419bb665eefde376790b17188410960f
Jettison <ilmsmjettison@gmail.com>
2021-12-08 22:10:51 +0800

    Save

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ let counterTwo = document.querySelector('.counter2');
 let closeNav = document.querySelector('.fa-window-close');
 let openNav = document.querySelector('.fa-bars');
 let Nav = document.querySelector('.mobile-nav');
+let city = document.querySelector('.place');
 
 
 openNav.addEventListener('click', showNav);
@@ -20,3 +21,28 @@ function showNav() {
 function hideNav() {
     Nav.classList.remove('showNav');
 }
+
+let locations = ['aarhus', 'tokyo', 'seoul', 'manila'];
+imgBg.style.backgroundImage = 'url(/bgimgs/1.jpg)';
+counterOne.innerText = '1/4';
+counterTwo.innerText = '1/4';
+city.innerText = locations[0];
+
+arrowLeft.addEventListener('click', () => {
+    let num = +imgBg.style.backgroundImage.split('/')[2][0];
+    num == 1 ? num = 4 : num--;
+    imgBg.style.backgroundImage = `url(/bgimgs/${num}.jpg)`;
+    counterOne.innerText = `${num}/4`;
+    counterTwo.innerText = `${num}/4`;
+    city.innerText = `${locations[num - 1]}`;
+})
+
+arrowRight.addEventListener('click', () => {
+    let num = +imgBg.style.backgroundImage.split('/')[2][0];
+    num == 4 ? num = 1 : num++;
+    imgBg.style.backgroundImage = `url(/bgimgs/${num}.jpg)`;
+    counterOne.innerText = `${num}/4`;
+    counterTwo.innerText = `${num}/4`;
+    city.innerText = `${locations[num - 1]}`;
+})
+
