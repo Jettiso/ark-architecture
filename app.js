830610539b73d9ee9fcacc7029c48714fa9cbb5f
Jettison <ilmsmjettison@gmail.com>
2021-12-08 21:29:29 +0800

    building js

--- app.js
+++ app.js
@@ -3,3 +3,13 @@ let arrowRight = document.querySelector('.fa-arrow-right');
 let arrowLeft = docuemnt.querySelector('.fa-arrow-left');
 let counterOne = document.querySelector('.counter');
 let counterTwo = document.querySelector('.counter2');
+let closeNav = document.querySelector('.fa-window-close');
+let openNav = document.querySelector('.fa-bars');
+
+openNav.addEventListener('click', );
+
+
+
+function showNav(e) {
+    e.target.classList.add('showNav');
+}
